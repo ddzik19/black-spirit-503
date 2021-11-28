77d1fb31df9fe408d43c1631b4be86d536515ecf
ddzik19 <ddzik09@gmail.com>
2021-11-28 11:38:44 +0000

    Bot Response
    The bot finally respongs, was missing an intent : Intents.FLAGS.GUILD_MESSAGES
    
    - embed does not work still.

--- bot/bot.js
+++ bot/bot.js
@@ -4,7 +4,7 @@ const {
     Intents
 } = require('discord.js');
 const client = new Client({
-    intents: [Intents.FLAGS.GUILDS]
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
 });
 const fs = require('fs');
 client.commands = new Discord.Collection();
@@ -24,19 +24,20 @@ client.on("ready", () => {
 const prefix = "!";
 client.on("message", msg => {
     // array of arguments, used for different kinds of functions
-    const args = [].msg.content.trim().split(' ');
+    const args = msg.content.trim().split(' ');
     const command = args.shift().toLowerCase();
-    
     // if the bot is the message author then return
     if (msg.author.bot) return
 
     if (msg.content === "!ping") {
         msg.channel.send("pong")
+        console.log(msg.content)
     }
 
     if (command === "!recipe") {
         msg.channel.send(`Recipe: ${args[0]}, ${args[1]}`);
         //client.commands.get(command).execute(command, args, Discord);
+        console.log("im here in recipe command!")
     }
 
     // if the user wants to search commands that contain

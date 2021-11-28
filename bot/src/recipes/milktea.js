771a824c638542142f2605ca7f97c6e7c7ec0c4f
ddzik19 <ddzik09@gmail.com>
2021-11-28 17:59:36 +0000

    Embed Image
    added thumbnail to the embed.

--- bot/src/recipes/milktea.js
+++ bot/src/recipes/milktea.js
@@ -8,10 +8,12 @@ module.exports = {
     name: "milktea",
     description: 'Milk Tea recipe, typically used for Imperial Cooking Boxes.',
     execute(msg, args) {
+        // thumbnail image from: https://bddatabase.net/us/recipe/132/
         const newEmbed = new MessageEmbed()
             .setColor('#304281')
             .setTitle('Milk Tea')
             .setDescription('the official bdo database milk tea description here <===')
+            .setThumbnail('https://bddatabase.net/items/new_icon/03_etc/07_productmaterial/00009263.png')
             .addFields({
                 name: 'Milk',
                 value: '3',

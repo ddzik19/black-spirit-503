38967aa3c39fbcbb9b6f8f392ac25798256c9726
ddzik19 <ddzik09@gmail.com>
2021-12-12 21:24:18 +0000

    Revert "Update milktea.js"
    This reverts commit 10898f290712ac3363d13cb2488bd6e6fdf01d74.

--- bot/src/recipes/milktea.js
+++ bot/src/recipes/milktea.js
@@ -1,47 +1,39 @@
 const {
+    Client,
+    Attachment,
+    Message,
     MessageEmbed
 } = require("discord.js");
 module.exports = {
     name: "milktea",
     description: 'Milk Tea recipe, typically used for Imperial Cooking Boxes.',
     execute(msg, args) {
-        let m = 3
-        let h = 3
-        let f = 2
-        let t = 2
-        if (args != null) {
-            m = (3 * args)
-            h = (3 * args)
-            f = (2 * args)
-            t = (2 * args)
-        }
-
         // thumbnail image from: https://bddatabase.net/us/recipe/132/
         const newEmbed = new MessageEmbed()
             .setColor('#304281')
             .setTitle('Milk Tea')
-            .setDescription('NOTE: In the Black Desert the craft is heavily affected by your skill level. At higher skill levels you can use less materials and get more products.')
+            .setDescription('the official bdo database milk tea description here <===')
             .setThumbnail('https://bddatabase.net/items/new_icon/03_etc/07_productmaterial/00009263.png')
-            .addFields({ // the fields with the names and amount of ingredients
+            .addFields({
                 name: 'Milk',
-                value: m.toString(),
+                value: '3',
                 inline: true
             }, {
                 name: 'Honey',
-                value: h.toString(),
+                value: '3',
                 inline: true
             }, {
-                name: 'Flour',
-                value: f.toString(),
+                name: 'flour',
+                value: '2',
                 inline: true
             }, {
                 name: 'Tea With Fine Scent',
-                value: t.toString(),
+                value: '2',
                 inline: true
             }, );
-        // creates the embed and sends it to the discord channel
         msg.channel.send({
             embeds: [newEmbed]
         });
+        //msg.channel.send(newEmbed);
     }
 }
